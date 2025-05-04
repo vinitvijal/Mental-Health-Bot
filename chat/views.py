ffe7416efe419a86294aabc1a1e5316714cb3864
Vinu <vineet.22566@sscbs.du.ac.in>
2025-05-04 15:58:07 +0530

    urls and view setup

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('chat/', views.chat_with_bot, name='chat'),
+]
